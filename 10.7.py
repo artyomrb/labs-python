5548e288cba46ceffa00ba7068ea24df1b98fc59
artyomrb <71343247+artyomrb@users.noreply.github.com>
2020-12-09 11:40:48 +0300

    Delete 10.7.py

--- 10.7.py
+++
@@ -1,2 +0,0 @@
-text = 'мой дядя самых честных правил, Когда не в шутку занемог, Он уважать себя заставил И лучше выдумать не мог'
-print(' '.join([ word for word in text.split() if not word.startswith('м') ]))
